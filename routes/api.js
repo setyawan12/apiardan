04ce887e830e08fb28eb50e9d646b693cbfd2533
ardans2003 <bocahgendut005@gmail.com.com>
2021-03-25 06:07:05 +0700

    update

--- routes/api.js
+++ routes/api.js
@@ -1996,4 +1996,20 @@ router.get('/googleimg', async (req, res, next) => {
 })
 })
 
+router.get('/nomoracak', async (req, res, next) => {
+        var apikeyInput = req.query.apikey
+            
+            
+	if(!apikeyInput) return res.json(loghandler.notparam)
+	if(apikeyInput != 'free') return res.json(loghandler.invalidKey)
+
+       var ardanacak = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69','70','71','72','73','74','75','76','77','78','79','80','81','82','83','84','85','86','87','88','89','90','91','92','93','94','95','96','97','98','99','100']
+       var ardanacake = ardanacak[Math.floor(Math.random() * ardanacak.length)]
+        res.json({ status : true, creator : `${creator}`, hasil : `${ardanacake}`})     
+         
+         .catch(e => {
+         	res.json('error ngab')
+})
+})
+
 module.exports = router
